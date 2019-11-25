5e7b041e94f89c0925bd2f73cc63e217c0e9fc6e
Jurij Sablic <jurij.sablic@gmail.com>
2019-11-25 01:23:10 +0100

    Python file that creates a dictionary of suspects that contains their body characteristics.

---
+++ suspects.py
@@ -0,0 +1,33 @@
+Eva = {
+    "gender": "female",
+    "race": "white",
+    "hair_color": "blonde",
+    "eye_color": "blue",
+    "facial_shape": "oval",
+}
+
+Larisa = {
+    "gender": "female",
+    "race": "white",
+    "hair_color": "brown",
+    "eye_color": "brown",
+    "facial_shape": "oval",
+}
+
+Matej = {
+    "gender": "male",
+    "race": "white",
+    "hair_color": "black",
+    "eye_color": "blue",
+    "facial_shape": "oval"
+}
+
+Miha = {
+    "gender": "male",
+    "race": "white",
+    "hair_color": "brown",
+    "eye_color": "green",
+    "facial_shape": "square",
+}
+
+suspects = {"Eva": Eva, "Larisa": Larisa, "Matej": Matej, "Miha": Miha}
