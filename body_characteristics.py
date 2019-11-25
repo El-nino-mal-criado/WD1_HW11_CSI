75b9b85faf0d2a9f651fad614352fa9fd6e7a10d
Jurij Sablic <jurij.sablic@gmail.com>
2019-11-25 01:18:39 +0100

    python file that creates a dictionary of body characteristics matching them to the corresponding dna sequences

---
+++ body_characteristics.py
@@ -0,0 +1,31 @@
+hair_color = {
+    "black": "CCAGCAATCGC",
+    "brown": "GCCAGTGCCG",
+    "blond": "TTAGCTATCGC"
+}
+
+facial_shape = {
+    "square": "GCCACGG",
+    "round": "ACCACAA",
+    "oval": "AGGCCTCA"
+}
+
+eye_color = {
+    "blue": "TTGTGGTGGC",
+    "green": "GGGAGGTGGC",
+    "brown": "AAGTAGTGAC"
+}
+
+gender = {
+    "female": "TGAAGGACCTTC",
+    "male": "TGCAGGAACTTC"
+}
+
+race = {
+    "white": "AAAACCTCA",
+    "black": "CGACTACAG",
+    "asian": "CGCGGGCCG"
+}
+
+body_characteristics = {"hair_color": hair_color, "facial_shape": facial_shape, "eye_color": eye_color,
+                        "gender": gender, "race": race}
